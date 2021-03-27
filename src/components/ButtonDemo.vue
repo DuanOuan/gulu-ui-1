ccddfd0d86a90a158f8a79444923a27695f621e5
chensilin <17570315440@163.com>
2021-03-27 12:25:59 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,3 +1,12 @@
 <template>
   <div>button文档</div>
+  <div>
+    <button>你好</button>
+  </div>
 </template>
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: { Button },
+};
+</script>
